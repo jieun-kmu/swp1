289c0eacc2631fec514f3ed61917261072dcd065
jieun-kmu <qwldms25@kookmin.ac.kr>
2020-06-26 18:52:12 +0900

    Issue #4 Move WSGI source files into new directory calc

---
+++ calc/template.py
@@ -0,0 +1,20 @@
+html = """
+<html>
+    <body>
+	<form action="">
+	    <p>
+		a = <input type="number" name="a" 
+	    </p>
+	    <p>
+		b = <input type="number" name="b"
+	    </p>
+	    <input type="submit">
+	</form>
+	<p>
+	    sum = %(sum)d</br>
+	    product = %(product)d
+	</p>
+    </body>
+</html>
+"""
+
---
+++ calc/work.py
@@ -0,0 +1,25 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    sum, product = 0, 0
+    if '' not in [a, b]:
+	a, b = int(a), int(b)
+	sum = a + b
+	product = a * b
+    response_body = html % {
+	'a': a,
+	'b': b,
+	'sum': sum,
+	'product': product,
+    }
+
+    start_response('200 OK', [
+	('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
+
